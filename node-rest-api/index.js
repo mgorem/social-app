dd2f0fdf762f672e2e96965c442fb08f4d0bae98
mgorem <oremgitonga22@gmail.com>
2022-06-01 15:30:20 +0300

    update registration, authentication and update

--- node-rest-api/index.js
+++ node-rest-api/index.js
@@ -1,16 +1,19 @@
 const express = require ("express");
 const app = express();
-const mongoose = require ("mongoose");
-const dotenv = require ("dotenv");
-const helmet = require ("helmet");
-const morgan = require ("morgan");
+
+const mongoose = require ("mongoose"); // library to help create mongodb database users e.t.c
+const dotenv = require ("dotenv"); // library to hide confidential secret keys
+const helmet = require ("helmet"); // library to hide vulnerable info in header upon requests
+const morgan = require ("morgan"); // give more info about a request, time e.t.c
 
 // Routes
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 
+// To use dotenv
 dotenv.config();
 
+// MongoDB connection
 mongoose.connect(process.env.MONGO_URL, 
     // { useNewUrlParser: true, useUnifiedTopology: true },
     () => {
@@ -19,7 +22,7 @@ mongoose.connect(process.env.MONGO_URL,
     );
 
     // middleware
-    app.use(express.json());
+    app.use(express.json()); // Body parser, that parses info after a request is made
     app.use(helmet());
     app.use(morgan("common"));
 
