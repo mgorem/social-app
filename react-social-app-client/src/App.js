7e9a283a3f5908f9bd7587a133970be2cf2ea902
mgorem <oremgitonga22@gmail.com>
2022-06-02 19:48:20 +0300

    add component folders and stylesheets

--- react-social-app-client/src/App.js
+++ react-social-app-client/src/App.js
@@ -1,10 +1,9 @@
-import Topbar from "./components/topbar/Topbar";
 import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div>
-      <Topbar />
+      <Home />
     </div>
   );
 }
--- react-social-app-client/src/pages/home/Home.js
+++ react-social-app-client/src/pages/home/Home.js
@@ -1,7 +1,16 @@
+import Topbar from "../../components/topbar/Topbar";
+// import Leftbar from "../../components/leftbar/Leftbar";
+// import Feed from "../../components/feedbar/Feed";
+// import Rightbar from "../../components/rightbar/Rightbar";
+
+
 export default function Home() {
     return (
         <div>
-            <p>This is Homepage</p>
+            <Topbar />
+            {/* <Leftbar />
+            <Feed />
+            <Rightbar /> */}
         </div>
     )
 }
