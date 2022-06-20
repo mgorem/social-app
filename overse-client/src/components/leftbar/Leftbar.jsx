cd7caf35dc99f9a997b51a32ea38e1c045439025
mgorem <oremgitonga22@gmail.com>
2022-06-20 15:23:13 +0300

    style left side bar

--- overse-client/src/components/leftbar/Leftbar.jsx
+++ overse-client/src/components/leftbar/Leftbar.jsx
@@ -1,12 +1,16 @@
 import './leftbar.css'
-
 import React from 'react'
-
+// import RssFeedIcon from '@mui/icons-material/RssFeed'
 const Leftbar = () => {
   return (
     <div className='leftContainer'>
         <div className="leftBarWrapper">
-          .
+          {/* <ul className="leftBarList">
+            <li className="leftBarListItem">
+            <RssFeedIcon/>
+            <span className="leftBarListItemText"></span>
+            </li>
+          </ul> */}
           Left Side Wrapper
         </div>
     </div>
