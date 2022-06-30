4df9c97c7615f05d5c2c6d387f168b0288416b16
mgorem <oremgitonga22@gmail.com>
2022-06-30 15:25:48 +0300

    solve database connection issue

--- overse-server/index.js
+++ overse-server/index.js
@@ -1,9 +1,9 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
-const mongoose = require ("mongoose");
-const dotenv = require ("dotenv");
-const helmet = require ("helmet");
-const morgan = require ("morgan");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 // Routes
 const userRoute = require("./routes/users");
@@ -11,21 +11,18 @@ const authRoute = require("./routes/auth");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, 
-    // { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-        console.log("Connected to MongoDB!")
-    }
-    );
+mongoose.connect(process.env.MONGO_URL, () => {
+  console.log("Databasee connected successfully!");
+});
 
-    // middleware
-    app.use(express.json());
-    app.use(helmet());
-    app.use(morgan("common"));
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
 
-    app.use("/api/users", userRoute);
-    app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
-    app.listen(8800, () => {
-      console.log("Backend server is running!")
-})
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
